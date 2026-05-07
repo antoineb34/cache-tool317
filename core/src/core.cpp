b01f103100ff17cae77e5a9b72ec0781cce05366
antoineb34 <antoineballeux015@gmail.com>
2026-05-07 11:48:21 -0400

    initial project structure: core, tool, client, cmake setup

---
+++ core/src/core.cpp
@@ -0,0 +1,2 @@
+// core library entry point
+// source files will be added here as we build out the library
---
+++ tool/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "cache-tool317" << std::endl;
+    return 0;
+}
